c523940003fcd6eb07bb4b662dc22d5723ba2c0b
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-04 23:25:52 -0700

    variables, if, else

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Printing the alphabets
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <='z', c++)
+		putchar (c);
+	for (c = 'A', c <= 'Z', c++)
+		putchar (c);
+		putchar ('\n');
+	
+	return (0);
+}
+
