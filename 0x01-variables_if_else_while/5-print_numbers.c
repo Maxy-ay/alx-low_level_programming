9ebc510fa895f070c4c3da527f1416b710da7ca5
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-05 05:48:48 -0700

    variables, if, else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a<= 9, a++)
+	{
+		printf(a);
+	}
+	printf('\n');
+	return (0);
+}
