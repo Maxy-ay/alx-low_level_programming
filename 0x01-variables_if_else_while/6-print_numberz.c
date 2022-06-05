1fbb524cbe092e6d3afa55cedeca902c9ab565c4
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-05 06:12:34 -0700

    variables, if, else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single numbers of base 10 without using printf func
+ *
+ * Return: Alway 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9, a++)
+	{
+		putchar(a);
+	}
+	putchar('\n')
+	return(0);
+}
