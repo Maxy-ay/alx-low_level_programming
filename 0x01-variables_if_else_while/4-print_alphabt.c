ca14b30cdee65a63d8d09d26fffbcfff85c65128
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-05 00:51:31 -0700

    variables, if, else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,13 @@
  */
 int main(void)
 {
-		char c;
-	
-		for (c = 'a'; c <= 'z'; c++)
-		{
-		if (c != 'e' && c != 'q')
-		putchar(c);
-		}
-		putchar('\n');
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	if (c != 'e' && c != 'q')
+	putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
