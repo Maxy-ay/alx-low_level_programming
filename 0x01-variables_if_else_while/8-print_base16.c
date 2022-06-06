e2b93c496dc09e4520384ff62b507d5174f77247
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-06 01:39:35 -0700

    variables, if, else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing all base 16 numbers in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 9; a++)
+	putchar(a);
+
+	for (a = 'a'; a <= 'f'; a++)
+	putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
