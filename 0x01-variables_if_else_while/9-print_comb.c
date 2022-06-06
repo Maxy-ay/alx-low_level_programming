9b89e5fc88df5c6931545a5f2ad456808b46f7f2
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-06 02:01:51 -0700

    variables, if, else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all combination of single numbers 
+ * i.e 0, 1, 2, ...,9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+	
+	{
+	putchar(num + '0');
+	if (num < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
