882120c354a7f9ce6224444ddae095c8af95fe7f
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-08 07:06:57 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char _putchar;
+
+	printf("_putchar\n");
+
+	return (0);
+}
+
+	
+
