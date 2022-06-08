31a6ee502d8de126a87456efc966051b03596460
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-08 08:49:55 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print the alphabet 10x
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int a, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (d = 97; d <= 122; d++)
+		{
+			_putchar(d)
+		}
+		_putchar('\n');
+	}
+}
