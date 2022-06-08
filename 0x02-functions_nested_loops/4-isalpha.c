197bcf1bbf9b50ffb14cd675a299933cfeff6b4b
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-08 12:52:27 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Returns 1 for upper or lowercase,
+ * else return 0 otherwise
+ *
+ * @c: character code in ASCII
+ *
+ * Return: Always 0 (success)
+ */
+int _isalpha(int c)
+{
+	if ( (c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
