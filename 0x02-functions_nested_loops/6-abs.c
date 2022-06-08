7cfd8b8b52de055d950e8d2c95ba97c24429fe8c
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-08 15:57:36 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of integer
+ *
+ * Return: Always 0 (success)
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+	_putchar('\n');
+}
