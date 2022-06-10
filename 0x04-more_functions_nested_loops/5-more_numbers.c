52bed6f284ee1bec0874261db5a53effc65d6cb4
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-06-09 18:55:48 -0700

    functions, nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,6 +15,9 @@ void more_numbers(void)
 		for (c = 0; c <= 14; c++)
 			{
 				if (c > 9)
-				_putchar((c / 10) + '0');							_putchar((c % 10) + '0');						}
-		_putchar('\n');								}
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}		
+			_putchar('\n');
+	}
 }
