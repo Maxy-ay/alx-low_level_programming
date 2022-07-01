5aaa2231567ef4cb9bf3bf8057364b78a9bbfbbd
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-07-01 09:45:56 -0700

    malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,51 +17,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	unsigned int j;
 
-
-
 	if (s1 == NULL)
-
-			s1 = "";
-
+		s1 = "";
 	if (s2 == NULL)
-
-			s2 = "";
-
+		s2 = "";
 	i = 0;
-
 	while (s1[i] != '\0')
-
-			i++;
-
+		i++;
 	strDup = malloc(sizeof(char) * (i + n + 1));
-
 	if (strDup == NULL)
-
-			return (NULL);
-
+		return (NULL);
 	i = j = 0;
-
 	while (s1[i] != '\0')
-
 	{
-
-			strDup[i] = s1[i];
-
-				i++;
-
+		strDup[i] = s1[i];
+		i++;
 	}
-
 	while (j < n && s2[j] != '\0')
-
 	{
-
-			strDup[i] = s2[j];
-
-				i++, j++;
-
+		strDup[i] = s2[j];
+		i++, j++;
 	}
-
 	strDup[i] = '\0';
-
 	return (strDup);
 }
