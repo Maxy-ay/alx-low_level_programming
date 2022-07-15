a5b570a791f1a7ecacc8764cd3f7715c19b65d71
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-07-15 00:17:29 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * @_strlen_recursion: - finds the length of a string.
+/**
+ * _strlen_recursion: - finds the length of a string.
  * @s: pointer to string
  * Return: length of sring
  **/
