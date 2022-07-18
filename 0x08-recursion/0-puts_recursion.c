35af19714c0a492d382c34713993b7300908e824
Ogheneyoma Aghoghovbia <maxwellaghoghovbia@gmail.com>
2022-07-17 21:15:21 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion: prints a string, followed by a new line
+ * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to string
  * Return: nothing
- */
+ **/
 
 void _puts_recursion(char *s)
 {
